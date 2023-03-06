004575f001c3f33fd1b6d1858c04d5ca013c4ff2
Yogesh <yogesh.kumar@brightchamps.com>
2023-03-07 00:04:50 +0530

    sign up page init

--- lib/Views/Login/SignupPage.dart
+++ lib/Views/Login/SignupPage.dart
@@ -10,11 +10,19 @@ class SignupPage extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<SignupPage> {
+  Container _topContent() {
+  return Container( child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [ Image.asset('assest/arrowBack.png', height: 40, width: 40,), Text('Sign Up', style: TextStyle(fontSize: 18, color: Colors.black, letterSpacing: 0.5),)],));
+}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container( child: Text('sdvfdv'),),
+        body: SafeArea(child:  Container( 
+          padding: EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20),
+          child: Column(
+             crossAxisAlignment: CrossAxisAlignment.start,
+            children: [_topContent()],),)),
       ),
     );
   }
